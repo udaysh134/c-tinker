9e2220ead4a4dadf7125a9d55d601de48aa137de
Uday Shukla <udaysh8351@gmail.com>
2025-11-04 11:35:27 +0530

    Create 02_greatestNum.c
    Added 02_greatestNum.c in Mini Programs to find the greatest of three provided numbers from the terminal

---
+++ src/udaysh134/Mini Programs/02_greatestNum.c
@@ -0,0 +1,22 @@
+/*
+----------------------------------------------------------------------------------------------------
+Finding the greatest of three numbers
+----------------------------------------------------------------------------------------------------
+*/
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a > b && a > c) {
+        printf("%d is the greatest.\n", a);
+    } else if (b > a && b > c) {
+        printf("%d is the greatest.\n", b);
+    } else {
+        printf("%d is the greatest.\n", c);
+    }
+    
+    return 0;
+}
