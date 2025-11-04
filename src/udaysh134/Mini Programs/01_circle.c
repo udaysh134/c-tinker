c035903cae948057b541abd5828dada5bc8ed477
Uday Shukla <udaysh8351@gmail.com>
2025-11-04 11:30:15 +0530

    Created "Mini Programs" folder to store small basic programs
    Created a folder to store basic C programs for others to reference and learn from the repo. Added 01_circle.c  to calculate area and circumference of a circle with any given radius.

---
+++ src/udaysh134/Mini Programs/01_circle.c
@@ -0,0 +1,20 @@
+/*
+----------------------------------------------------------------------------------------------------
+Calculating the Area and Circumference of a circle
+----------------------------------------------------------------------------------------------------
+*/
+#include <stdio.h>
+#define PI 3.1416
+
+int main() {
+    float r, area, circum;
+    printf("Enter radius of circle: ");
+    scanf("%f", &r);
+
+    area = PI * r * r;
+    circum = 2 * PI * r;
+
+    printf("Area of circle = %.2f\n", area);
+    printf("Circumference of circle = %.2f\n", circum);
+    return 0;
+}
