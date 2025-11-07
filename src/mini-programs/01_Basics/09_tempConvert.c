d23deb531e4a0c4692b4150780d39c35711e8c86
Uday Shukla <udaysh8351@gmail.com>
2025-11-07 20:49:17 +0530

    Create 09_tempConvert.c
    Added a basic program to convert temperature from Celcius to Fahrenheit.

---
+++ src/mini-programs/01_Basics/09_tempConvert.c
@@ -0,0 +1,24 @@
+/**
+ * -----------------------------------------------------------------------------
+ * Program : 09_tempConvert.c
+ * Purpose : Convert temperature from Celcius to Fahrenheit
+ * Authors : [Uday]
+ * Date : Nov 07, 2025
+ * -----------------------------------------------------------------------------
+ */
+
+#include <stdio.h>
+
+int main() {
+    float c, f;
+
+    printf("Enter temperature in Celcius: ");
+    scanf("%f", &c);
+
+    // Using the formula
+    f = (c * 9 / 5) + 32;
+
+    // Using ".2f" means we're allowing only 2 digits after decimal to be printed
+    printf("Temperature in Fahrenheit = %.2f\n", f);
+    return 0;
+}
