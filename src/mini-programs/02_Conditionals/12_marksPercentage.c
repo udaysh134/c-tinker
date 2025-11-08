88d96a639005a41d2fa90d585d1dcc94035596f0
Uday Shukla <udaysh8351@gmail.com>
2025-11-08 20:19:49 +0530

    Added 12_marksPercentage.c
    Added a program to do basic arithmetic and calculate the Total Marks and Percentage obtained from the provided scores

---
+++ src/mini-programs/02_Conditionals/12_marksPercentage.c
@@ -0,0 +1,25 @@
+/**
+ * -----------------------------------------------------------------------------
+ * Program : 12_marksPercentage.c
+ * Purpose : Calculate the Total Marks and Percentage obtained from the provided scores
+ * Authors : [Uday]
+ * Date : Nov 08, 2025
+ * -----------------------------------------------------------------------------
+ */
+
+#include <stdio.h>
+
+int main() {
+    float m1, m2, m3, m4, m5, sum, per;
+
+    printf("Enter marks of 5 subjects: ");
+    scanf("%f %f %f %f %f", &m1, &m2, &m3, &m4, &m5);
+
+    sum = m1 + m2 + m3 + m4 + m5;
+    per = (sum / 500) * 100; // Basic arithmetic to find percentage
+
+    printf("Total Marks = %.2f\n", sum);
+    printf("Percentage = %.2f%%\n", per);
+
+    return 0;
+}
