8bf0a516ede6e11a1873b58dda4c3570aa358d26
Uday Shukla <udaysh8351@gmail.com>
2025-11-17 17:17:06 +0530

    Added 19_strReverse.c
    Added a new program to use library function and reverse the content of a string input, taken from the user.

---
+++ src/mini-programs/05_Strings/19_strReverse.c
@@ -0,0 +1,23 @@
+/**
+ * -----------------------------------------------------------------------------
+ * Program : 19_strReverse.c
+ * Purpose : Reverse the content of a string using library function.
+ * Authors : [Uday]
+ * Date : Nov 17, 2025
+ * -----------------------------------------------------------------------------
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+
+    gets(str); // Reads the input string. gets() is another alternative to get input
+    strrev(str); // Reverse the string using strrev() from <string.h>
+
+    printf("Reversed string: %s\n", str);
+    return 0;
+}
