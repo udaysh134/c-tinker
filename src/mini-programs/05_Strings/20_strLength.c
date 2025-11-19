a7377fa73fea419cf71e1f49a3ba83f19b18eae4
Uday Shukla <udaysh8351@gmail.com>
2025-11-19 13:15:38 +0530

    Added 20_strLength.c
    Added a program to find the length of a string without using any library function, taking advantage of 'while' loop learnt earlier

---
+++ src/mini-programs/05_Strings/20_strLength.c
@@ -0,0 +1,25 @@
+/**
+ * -----------------------------------------------------------------------------
+ * Program : 20_strLength.c
+ * Purpose : Find the length of a string without using any library function.
+ * Authors : [Uday]
+ * Date : 
+ * -----------------------------------------------------------------------------
+ */
+
+#include <stdio.h>
+
+int main() {
+    int i = 0; // Variable to count the number of characters
+    char str[100];
+
+    printf("Enter a string: ");
+    gets(str);
+
+    // We'll use loop to iterate through every element of the string (we know strings are arrays in C)
+    // Loop the string until the null terminator is found
+    while (str[i] != '\0') { i++; }
+
+    printf("Length of the string = %d\n", i);
+    return 0;
+}
